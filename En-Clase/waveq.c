2e1a1d95d9d59d235d215a26c7fa8a237579fb39
Juan Rafael Álvarez Velásquez <juanra31a@gmail.com>
2014-04-25 14:07:18 -0500

    avances tarea

--- En-Clase/waveq.c
+++ En-Clase/waveq.c
@@ -27,45 +27,77 @@ void copy(FLOAT *u, FLOAT *u_past, int n_points){
 int main(int argc, char **argv){
 
     
-    int n_points = 80; //n_points in x
-    int n_t = 300;
+    int n_points = 1000; //n_points in x
+    int n_t = 350;
     
     FLOAT *x;
     FLOAT *u;
     FLOAT *u_past;
+    FLOAT *u_initial;
+    FLOAT *u_future;
+    FLOAT *u_present;
+    FLOAT r;
     FLOAT dx;
     FLOAT dt;
     FLOAT c = 1.0;
     x = get_memory(n_points);
     u = get_memory(n_points);
+    u_initial = get_memory(n_points);
     u_past = get_memory(n_points);
+    u_future = get_memory(n_points);
+    u_present = get_memory(n_points);
     int i,j;
     
     x[0] = 0.0;
     
     for (i=0; i<n_points; i++) {
-
-        
-        
-        u[i] = 1.0;
         
         if (i!=0) {
-        x[i] = x[i-1] + (2.0)/n_points;
-            
-        if(x[i]<1.25 && x[i]>0.75){
-            u[i] = 2.0;
-            }
+        x[i] = x[i-1] + (1.0)/n_points;
         }
+        
+        u_initial[i] = exp(-((x[i]-0.3)*(x[i]-0.3))/0.01);
     }
     
+    
+    /*find the first iteration*/
+    
     dx = x[1]-x[0];
-    dt = 0.001;
-    printf("%f\n", c*dt/dx);
-    for(i=0;i<n_t;i++){
-        copy(u, u_past, n_points);
-        for(j=1;j<n_points-1;j++){
-            u[i] = u_past[i] - c*dt/dx * (u_past[i]-u_past[i-1]);
+    dt = 0.0005;
+    r = c * dt/dx;
+    
+    /*r should be less than 1.0 in this scheme in order to be numerically stable*/
+    printf("%f\n",r);
+    
+    /*Fixed boundary condition*/
+    u_initial[0] = 0.0;
+    u_initial[n_points-1] = 0.0;
+    
+    u_future[0] = 0.0;
+    u_future[n_points - 1] = 0.0;
+    
+    
+    for (i=1; i<n_points - 1; i++) {
+        u_future[i] = u_initial[i] + (pow(r,2)/2.0)*(u_initial[i+1]-2.0*u_initial[i]+ u_initial[i-1]);
+    }
+    //Variable to hold the previous value
+    copy(u_initial, u_past, n_points);
+    //Variable to hold the present value
+    copy(u_future, u_present, n_points);
+    
+    
+    /*Next iterations:*/
+    
+    for (j=0; j<n_t; j++) {
+        printf("Voy en el paso %d \n",j);
+        for (i=1; i<n_points; i++) {
+            u_future[i] = (2.0*(1.0-pow(r,2)))*u_present[i] - u_past[i] + pow(r,2)*(u_present[i+1] + u_present[i-1]);
+            printf("%f\n",u_present[i]);
         }
+        //Variable to hold the previous value
+        copy(u_present, u_past, n_points);
+        //Variable to hold the present value
+        copy(u_future, u_present, n_points);
     }
     
     
@@ -73,10 +105,8 @@ int main(int argc, char **argv){
     
     
     for(i=0;i<n_points;i++){
-        fprintf(in,"%f %f \n",x[i],u[i]);
+        fprintf(in,"%f %f %f %f \n",x[i],u_initial[i], u_future[i],u_present[i]);
     }
-    
-    
     
     fclose(in);
     
